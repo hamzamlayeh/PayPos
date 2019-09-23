8703b1811d848b8d63769aec4c6c7a6c24dc4b7d
hamza <hamzamlayeh@outlook.com>
2019-09-23 12:29:01 +0100

    firest comit

---
+++ app/src/main/java/com/example/paypos/Authentification.java
@@ -0,0 +1,17 @@
+package com.example.paypos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Authentification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentification);
+    }
+
+    public void Login(View view) {
+    }
+}
